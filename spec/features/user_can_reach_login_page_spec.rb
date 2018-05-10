07c3f67f09d0663091d491331616f863496b3e4e
Nikhil Shahi <shniks@gmail.com>
2018-05-10 12:23:45 -0600

    Initial commit

---
+++ spec/features/user_can_reach_login_page_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require './spec/support/omniauth.rb'
+
+describe 'When I visit the root' do
+  scenario 'I see the logging in status code' do
+    visit '/'
+
+    expect(page.status_code).to eq(200)
+
+    click_link "Sign in with GitHub"
+
+    expect(current_path).to eq("/")
+    expect(page).to have_content('Nikhil')
+    expect(page).to have_link('Logout')
+  end
+end
---
+++ spec/features/user_can_see_own_details_on_page_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require './spec/support/omniauth.rb'
+
+describe 'User can see own information' do
+  scenario 'Can see account information' do
+    # When I visit '/'
+    visit '/'
+
+    click_on "Sign in with GitHub"
+
+    expect(current_path).to eq('/')
+
+    click_on 'Profiles'
+
+    username = 'Shniks'
+    expect(current_path).to eq("/#{username}")
+    expect(page).to have_content('Nikhil Shahi')
+    expect(page).to have_content('Shniks')
+    expect(page).to have_content('Denver, Colorado')
+    expect(page).to have_content('Overview')
+    expect(page).to have_content('Repositories')
+    expect(page).to have_content('Followers')
+    expect(page).to have_content('Following')
+    expect(page).to have_xpath
+    # I can see my profile pic, starred repos, followers and following
+  end
+end
